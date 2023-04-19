a63c0d0ea8c7bc967eeb6d83f35442277fbb091e
Lowell Tarek Abbott Vidal <lowell.abbott@gmail.com>
2023-04-20 01:38:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
- ethspam | versus --stop-after=100 --concurrency=5 'http://erigon-mainnet:8545/'
-
+ethspam | versus --stop-after=20 --concurrency=400000 'http://erigon-matic:8545/'
+ethspam | versus --stop-after=50 --concurrency=1000000 'http://erigon-matic:8545/'
+ethspam | versus --stop-after=100 --concurrency=2000000 'http://erigon-matic:8545/'
